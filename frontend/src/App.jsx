469b21c14c7d34e62cbe9c3ad39e9ff832039429
Joseph <indiekaj@gmail.com>
2024-07-18 00:54:22 +0300

    react frontend set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-red-400 text-white"> App</div>
+  );
+}
+
+export default App

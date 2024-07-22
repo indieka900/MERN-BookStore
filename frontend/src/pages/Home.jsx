0012c7453d152f3873cb4b3911b742b135c459f0
Joseph <indiekaj@gmail.com>
2024-07-23 00:24:18 +0300

    clean code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -28,19 +28,21 @@ export const Home = () => {
   return (
     <div className="p-4">
       <div className="flex justify-center items-center gap-x-4">
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType("table")}
-        >
-          Table
-        </button>
-      
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType("card")}
-        >
-          Card
-        </button>
+        {showType === "table" ? (
+          <button
+            className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+            onClick={() => setShowType("card")}
+          >
+            Card
+          </button>
+        ) : (
+          <button
+            className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+            onClick={() => setShowType("table")}
+          >
+            Table
+          </button>
+        )}
       </div>
       <div className="flex justify-between items-centre">
         <h1 className="text-3xl my-8">Books List</h1>
